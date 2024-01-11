2b4f0cd1dda87ec9cce6c43b2e9078ccd7a44ca7
Dylan Hoff <dylanhoff@Dylans-MacBook-Pro-3.local>
2024-01-10 17:30:13 -0800

    Fix indentation in prompt

--- assistant/tour_generation_prompt.py
+++ assistant/tour_generation_prompt.py
@@ -5,66 +5,66 @@ from tour import Tour
 
 
 _TOUR_ITINERARY_PROMPT_TEMPLATE_TEXT = """
-    Create an itinerary for a walking tour.
-    The tour should be described with a title and short description.
-    Start location: {start_location}
-    Approximate total distance in miles: {distance_mi}
+Create an itinerary for a walking tour.
+The tour should be described with a title and short description.
+Start location: {start_location}
+Approximate total distance in miles: {distance_mi}
 
-    {theme_instructions}
+{theme_instructions}
 
-    Stops
-    - There should be approximately {approx_stops} stops along the tour.
-    - Populate the entire list of stops with valid stops.
-    - The location of each stop must be as specific as possible so there is no ambiguity 
-    about where to stand (e.g. if the location is a building, the location must 
-    specify exactly where around or in the building the users should stand to 
-    give the related information the best context).
-    - If the start location is an interesting place, it should be the first stop.
-    - The number of stops can be slightly higher or lower depending on how much 
-    information there is to cover in the area given the distance.
-    - These stops should be listed in the order in which they should be visited 
-    with the order determined by travel efficiency and/or the logical flow of 
-    information presented from one stop to the next.
-    - Each stop should contain a teaser that the user will read before traveling 
-    to it foreboding the information that will be covered. (e.g. for the first 
-    stop: "First, we will go to x to learn about y", for middle stops: "Next, 
-    we will visit x to see y", for the last stop: "Finally, we're headed to x, 
-    where y occurred").
-    - The background of each stop should be in paragraph format and provide 
-    interesting information about the location. The length of the background 
-    should depend on the amount of relevant information.
-    - The key facts of each stop should cover other interesting facts that are 
-    not covered in the background. The number of key facts should depend on the 
-    amount of relevant information.
+Stops
+- There should be approximately {approx_stops} stops along the tour.
+- Populate the entire list of stops with valid stops.
+- The location of each stop must be as specific as possible so there is no ambiguity 
+about where to stand (e.g. if the location is a building, the location must 
+specify exactly where around or in the building the users should stand to 
+give the related information the best context).
+- If the start location is an interesting place, it should be the first stop.
+- The number of stops can be slightly higher or lower depending on how much 
+information there is to cover in the area given the distance.
+- These stops should be listed in the order in which they should be visited 
+with the order determined by travel efficiency and/or the logical flow of 
+information presented from one stop to the next.
+- Each stop should contain a teaser that the user will read before traveling 
+to it foreboding the information that will be covered. (e.g. for the first 
+stop: "First, we will go to x to learn about y", for middle stops: "Next, 
+we will visit x to see y", for the last stop: "Finally, we're headed to x, 
+where y occurred").
+- The background of each stop should be in paragraph format and provide 
+interesting information about the location. The length of the background 
+should depend on the amount of relevant information.
+- The key facts of each stop should cover other interesting facts that are 
+not covered in the background. The number of key facts should depend on the 
+amount of relevant information.
 
-    Stop Activities
-    - For some of the stops, add activities to be perfomed.
-    - A stop can have no activities, a single activity, or multiple activities 
-    depending on how well activities would fit the stop.
-    - An activity can be one of the following types and its content must match 
-    the corresponding json schema exactly.
+Stop Activities
+- For some of the stops, add activities to be perfomed.
+- A stop can have no activities, a single activity, or multiple activities 
+depending on how well activities would fit the stop.
+- An activity can be one of the following types and its content must match 
+the corresponding json schema exactly.
 
-    Activity Types
-    Type: trivia
-    Description: A trivia question related to the stop with 4 options for the 
-    answer and one being correct, indicated by correct_answer_index
-    Content Schema: [
-        question: str,
-        answer_options: List[str],
-        correct_answer_index: int,
-    ]
+Activity Types
+Type: trivia
+Description: A trivia question related to the stop with 4 options for the 
+answer and one being correct, indicated by correct_answer_index
+Content Schema: [
+    question: str,
+    answer_options: List[str],
+    correct_answer_index: int,
+]
 
-    The response must be valid json. Do not output invalid nested quotes.
+The response must be valid json. Do not output invalid nested quotes.
 
-    {format_instructions}
+{format_instructions}
 """
 
 _THEME_INSTRUCTIONS_TEMPLATE_TEXT = """
-    Theme
-    - The theme of this tour is {theme}.
-    - The title and description of the tour should be relevant to the theme.
-    - The stops chosen and their accompanying information should be relevant to the theme.
-    - The tour stop activities chosen and their content should be relevant to the theme.
+Theme
+- The theme of this tour is {theme}.
+- The title and description of the tour should be relevant to the theme.
+- The stops chosen and their accompanying information should be relevant to the theme.
+- The tour stop activities chosen and their content should be relevant to the theme.
 """
 
 
